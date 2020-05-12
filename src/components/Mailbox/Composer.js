00ae563d60a9829f98089342a0248f93d5979ce2
bidetaggle <bidetaggle@world>
2020-05-12 14:55:01 +0200

    Composer design

--- src/components/Mailbox/Composer.js
+++ src/components/Mailbox/Composer.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+import { TextField, Button, ThemeProvider } from '@material-ui/core';
+import theme from './../Theme'
+import './Composer.css'
 
 function Composer(){
-  return <div>composer</div>
-  // https://www.slatejs.org/examples/richtext
+
+  return (
+    <ThemeProvider theme={theme}>
+      <form id="composer">
+        <TextField
+          className="standard-basic"
+          label="Recipient"
+        />
+        <TextField
+          className="standard-basic"
+          label="Subject"
+        />
+        <TextField
+          className="standard-multiline-static"
+          label="Content"
+          multiline
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<i className="far fa-paper-plane"></i>}
+        >
+          Send
+        </Button>
+      </form>
+    </ThemeProvider>
+  )
 }
 export default Composer;
